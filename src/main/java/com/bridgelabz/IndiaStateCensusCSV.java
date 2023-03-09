3ef6f5bef4469a16eb01d0bb8a5b408800afe77c
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-03-09 20:37:49 +0530

    Indian State Census Analyser

---
+++ src/main/java/com/bridgelabz/IndiaStateCensusCSV.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IndiaStateCensusCSV {
+
+
+    public String state;
+
+    public int population;
+
+    public int areaInSqKm;
+
+    public int densityPerSqKm;
+
+    @Override
+    public String toString() {
+        return "IndiaCensusCSV{" +
+                "State='" + state + '\'' +
+                ", Population='" + population + '\'' +
+                ", AreaInSqKm='" + areaInSqKm + '\'' +
+                ", DensityPerSqKm='" + densityPerSqKm + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -0,0 +1,63 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import com.bridgelabz.CensusAnalyser;
+import com.bridgelabz.CensusAnalyserException;
+
+
+public class CensusAnalyserTest {
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_TYPE = "./src/test/resources/IndiaStateCensusData.txt";
+    private static final String SAMPLE_CSV_DELIMITERS = "./src/test/resources/SampleFileDelimiter.csv";
+    private static final String SAMPLE_CSV_HEADER = "./src/test/resources/SampleFileHeader.csv";
+
+    // India State Code Information
+    private static final String STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
+    private static final String WRONG_STATE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
+    private static final String WRONG_STATE_CSV_TYPE = "./src/test/resources/IndiaStateCode.txt";
+    private static final String SAMPLE_STATE_CODE_DELIMITER = "./src/test/resources/SampleStateCodeDelimiter.txt";
+    private static final String SAMPLE_STATE_CODE_HEADER = "./src/test/resources/SampleStateCodeHeader.csv";
+
+    CensusAnalyser censusAnalyser = new CensusAnalyser();
+
+
+    // UC1 - Test cases for Indian State Census Information
+    @Test
+    public void given_IndianCensusCSVFile_ReturnsCorrectRecords() throws CensusAnalyserException {
+        int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+        Assert.assertEquals(29, numOfRecords);
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WhenWrongType_ShouldThrowException() {
+        try {
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_TYPE);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WhenDelimiterIncorrect_ShouldThrowException() {
+        try {
+            censusAnalyser.loadIndiaCensusData(SAMPLE_CSV_DELIMITERS);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, e.type);
+        }
+    }
+
+}
